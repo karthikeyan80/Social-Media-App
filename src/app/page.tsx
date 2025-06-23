c08d299915b9d5b242b8377ffe0e514fdc55b73f
karthikeyan80 <karthikeyan40581@gmail.com>
2025-06-23 13:33:59 +0530

    Implemented google and email authentication using Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";   
 
 export default function Home() {
   return (
-   <div>
-    <p>Hello World!</p>
-   </div>
+    <div>
+      <SignedOut>
+         <SignInButton>
+          <button className="bg-gradient-to-l from-red-500 via-purple-400 hover:bg-gradient-to-r text-white font-bold py-2 px-4 rounded">Sign In</button>
+          </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   );
-}
+} 
